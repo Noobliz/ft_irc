0846c417685906961c357873fe7c7b314b51fd3b
Timothe Legrand <tilegran@1C7.42angouleme.fr>
2025-09-17 16:12:36 +0200

    MODE remaining

--- TIM/main.cpp
+++ TIM/main.cpp
@@ -2,7 +2,14 @@
 #include <sstream>
 #include <string>
 #include <stdexcept>
-void join(std::string cmd, std::stringstream *ss);
+void join(std::stringstream *ss, bool hasPrefix, std::string prefix);
+void privmsg(std::stringstream *ss, bool hasPrefix, std::string prefix);
+void pass(std::stringstream *ss);
+void nick(std::stringstream *ss, bool hasPrefix, std::string prefix);
+void user(std::stringstream *ss, bool hasPrefix, std::string prefix);
+void invite(std::stringstream *ss, bool hasPrefix, std::string prefix);
+void kick(std::stringstream *ss, bool hasPrefix, std::string prefix);
+void topic(std::stringstream *ss, bool hasPrefix, std::string prefix);
 
 std::string getPrefix(std::stringstream *ss, bool *hasPrefix){
 	std::string prefix;
@@ -20,26 +27,25 @@ void chooseCmd(std::string prefix, bool hasPrefix, std::stringstream *ss){
 
 	(void)prefix;
 	if(cmd == "PASS")
-			;//
-	else if(cmd == "NICK")
-			;//
-	else if(cmd == "USER")
-			;//
-	else if(cmd == "JOIN")
 	{
 		if (hasPrefix)
-			throw std::invalid_argument("Error: invalid syntax");
-		else
-			join(cmd, ss);
+			throw std::invalid_argument("Error: PASS cant have a prefix.");
+		pass(ss);
 	}
+	else if(cmd == "NICK")
+		nick(ss, hasPrefix, prefix);
+	else if(cmd == "USER")
+		user(ss, hasPrefix, prefix);
+	else if(cmd == "JOIN")
+		join(ss, hasPrefix, prefix);
 	else if(cmd == "PRIVMSG")
-			;//
+		privmsg(ss, hasPrefix, prefix);
 	else if(cmd == "KICK")
-			;//
+		kick(ss, hasPrefix, prefix);
 	else if(cmd == "INVITE")
-			;//
+		invite(ss, hasPrefix, prefix);
 	else if(cmd == "TOPIC")
-			;//
+		topic(ss, hasPrefix, prefix);
 	else if(cmd == "MODE")
 			;//
 	else
