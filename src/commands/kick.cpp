57cbde5e7cc3c5f876d2f01f891550a71bfd4c2b
Autman El Atmioui <ael-atmi@1D5.42angouleme.fr>
2025-09-25 14:15:53 +0200

    update kick

--- src/commands/kick.cpp
+++ src/commands/kick.cpp
@@ -2,80 +2,80 @@
 
 #include <Server.hpp>
 
-void    Server::kick(t_commandArgs & cArgs)
+void	Server::kick(t_commandArgs & cArgs)
 {
-    std::vector<std::string> targets;
-    std::string channel;
-    std::string msg = "";
-    std::string words;
-    int sscount = 0;
-    std::streampos ssPos;
+	std::vector<std::string> targets;
+	std::string channel;
+	std::string msg = "";
+	std::string words;
+	int sscount = 0;
+	std::streampos ssPos;
 	std::string err_feedback;
 
-    while (*cArgs.sstream >> words)
-    {
-        std::cout << "words :" << words << std::endl;
-        if (sscount == 0)
-        {
-            if (words[0] != '#')
+	while (*cArgs.sstream >> words)
+	{
+		std::cout << "words :" << words << std::endl;
+		if (sscount == 0)
+		{
+			if (words[0] != '#')
 			{
 				err_feedback = ERR_BADCHANMASK(cArgs.client->getNickname(), words);
 				if (send(cArgs.client->getFD(), err_feedback.c_str(), err_feedback.length(), 0) == -1)
 					throw std::runtime_error("send() failed");
-                throw std::invalid_argument("Error: channel name has to start with #.");
+				throw std::invalid_argument("Error: channel name has to start with #.");
 			}
-            channel = words;
-        }
-        else if (sscount == 1)
-        {
-            std::stringstream target(words);
-            while (std::getline(target, words, ','))
-                targets.push_back(words);
-            ssPos = cArgs.sstream->tellg();
-        }
-        else if (sscount == 2)
-        {
-            if (words[0] == ':')
-            {
-                msg = (cArgs.sstream->str()).substr(ssPos);
-                std::stringstream msgSStream(msg);
-                std::getline(msgSStream, msg, ':');
-                std::getline(msgSStream, msg);
-                msg.append("\n");
-                sscount++;
-                break;
-            }
-            msg = words;
-            msg.append("\n");
-        }
-        else if (sscount == 3)
-            throw std::invalid_argument("Error: too many arguments.");
-        sscount++;
-    }
-    if (sscount < 2)
-   {
+			channel = words;
+		}
+		else if (sscount == 1)
+		{
+			std::stringstream target(words);
+			while (std::getline(target, words, ','))
+				targets.push_back(words);
+			ssPos = cArgs.sstream->tellg();
+		}
+		else if (sscount == 2)
+		{
+			if (words[0] == ':')
+			{
+				msg = (cArgs.sstream->str()).substr(ssPos);
+				std::stringstream msgSStream(msg);
+				std::getline(msgSStream, msg, ':');
+				std::getline(msgSStream, msg);
+				msg.append("\n");
+				sscount++;
+				break;
+			}
+			msg = words;
+			msg.append("\n");
+		}
+		else if (sscount == 3)
+			throw std::invalid_argument("Error: too many arguments.");
+		sscount++;
+	}
+	if (sscount < 2)
+	{
 		err_feedback = ERR_NEEDMOREPARAMS(cArgs.client->getNickname(), "INVITE");
 		if (send(cArgs.client->getFD(), err_feedback.c_str(), err_feedback.length(), 0) == -1)
 			throw std::runtime_error("send() failed");
 		throw std::invalid_argument("Error: not enough arguments.");
-   }
-    std::cout << "Debug infos:" << std::endl;
-    std::cout << "Channel:" << channel << std::endl;
-    for (std::vector<std::string>::iterator it = targets.begin(); it != targets.end(); ++it)
-    {
-        std::cout << "Target:" << *it << std::endl;
-    }
-    std::cout << "Msg:" << msg << std::endl;
-    std::cout << std::endl;
-    if (cArgs.client->isAuth())
+	}
+	std::cout << "Debug infos:" << std::endl;
+	std::cout << "Channel:" << channel << std::endl;
+	for (std::vector<std::string>::iterator it = targets.begin(); it != targets.end(); ++it)
+	{
+		std::cout << "Target:" << *it << std::endl;
+	}
+	std::cout << "Msg:" << msg << std::endl;
+	std::cout << std::endl;
+	if (cArgs.client->isAuth())
 		doKick(*cArgs.client, targets, channel, msg);
-    else
-    {
-        err_feedback = ERR_NOTREGISTERED;
-        if (send(cArgs.client->getFD(), err_feedback.c_str(), err_feedback.length(), 0) == -1)
-            throw std::runtime_error("send() failed");
-        throw std::invalid_argument(err_feedback);
-    }
+	else
+	{
+		err_feedback = ERR_NOTREGISTERED;
+		if (send(cArgs.client->getFD(), err_feedback.c_str(), err_feedback.length(), 0) == -1)
+			throw std::runtime_error("send() failed");
+		throw std::invalid_argument(err_feedback);
+	}
 }
 
 void	Server::doKick(Client & client, std::vector<std::string> const &target, std::string const &chan, std::string const &msg)
@@ -128,13 +128,13 @@ void	Server::doKick(Client & client, std::vector<std::string> const &target, std
 		}
 		std::map<std::string, Client> &connectedClients = iteChan->second.getConnectedClients();
 		std::map<std::string, Client>::iterator ite2 = connectedClients.begin();
-		
+
 		feedback = KICK(client.getNickname(), client.getUserinfo().username, chan, target[i], msg);
-		
+
 		for (; ite2 != connectedClients.end(); ++ite2)
 		{
 			if (send(ite2->second.getFD(), feedback.c_str(), feedback.length(), 0) == -1)
-			throw std::runtime_error("send() failed");
+				throw std::runtime_error("send() failed");
 		}
 		//rm user from chan and chan from users
 		_clients[clientfd].removeChan(chan);
